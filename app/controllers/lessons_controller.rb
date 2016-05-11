9fae17a27e6e825546b3d41cf422d550f0fc66c8
Paul Yarabe <paulyarabe@post.harvard.edu>
2016-05-11 03:22:26 +0000

    debugging2

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,16 +1,16 @@
 class LessonsController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_lesson, only: [:show]
+  before_action :require_enrolled_for_current_lesson, only: [:show]
 
   def show
   end
 
   private
   
-  def require_authorized_for_current_lesson
-    if !current_user.enrolled_in?(current_lesson.section.course)
-      redirect_to course_path(current_lesson.section.course), :alert => 'Please enroll for this course first!'
+  def require_enrolled_for_current_lesson
+    if ! current_user.enrolled_in?(current_course)
+      return redirect_to course_path(current_course), alert: 'Please enroll for this course first!'
     end
   end
 
